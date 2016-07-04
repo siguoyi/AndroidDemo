db4ab48d0b1e351c683887e376cdd2f55e1605b1
siguoyi <believemine@163.com>
2016-07-04 16:22:03 +0800

    update  RxJavaTransformActivity with ButterKnife

--- app/src/main/java/com/example/chenlei1_iri/androiddemo/RxJavaTransformActivity.java
+++ app/src/main/java/com/example/chenlei1_iri/androiddemo/RxJavaTransformActivity.java
@@ -1,8 +1,8 @@
 package com.example.chenlei1_iri.androiddemo;
 
 import android.content.pm.ActivityInfo;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -12,19 +12,28 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import rx.Observable;
 import rx.Subscriber;
 import rx.functions.Action1;
 import rx.functions.Func1;
 
-public class RxJavaTransformActivity extends AppCompatActivity implements View.OnClickListener{
+public class RxJavaTransformActivity extends AppCompatActivity {
     private static final String TAG = RxJavaTransformActivity.class.getSimpleName();
-
-    private Button btn_map;
-    private Button btn_flatMap;
-    private Button btn_flatMap1;
-    private Button btn_filter;
-    private TextView tv_rx_text;
+    @BindView(R.id.btn_map)
+    Button btnMap;
+    @BindView(R.id.btn_flatmap)
+    Button btnFlatmap;
+    @BindView(R.id.btn_flatmap1)
+    Button btnFlatmap1;
+    @BindView(R.id.btn_filter)
+    Button btnFilter;
+    @BindView(R.id.btn_take)
+    Button btnTake;
+    @BindView(R.id.tv_rx_text)
+    TextView tvRxText;
 
     private StringBuilder sb;
     private Student[] student = new Student[3];
@@ -39,52 +48,19 @@ public class RxJavaTransformActivity extends AppCompatActivity implements View.O
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rx_java_transform);
+        ButterKnife.bind(this);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-
-        btn_map = (Button) findViewById(R.id.btn_map);
-        btn_flatMap = (Button) findViewById(R.id.btn_flatmap);
-        btn_flatMap1 = (Button) findViewById(R.id.btn_flatmap1);
-        btn_filter = (Button) findViewById(R.id.btn_filter);
-        tv_rx_text = (TextView) findViewById(R.id.tv_rx_text);
-
-        btn_map.setOnClickListener(this);
-        btn_flatMap.setOnClickListener(this);
-        btn_flatMap1.setOnClickListener(this);
-        btn_filter.setOnClickListener(this);
-
         sb = new StringBuilder();
 
         course1.add(new Student().new Course(0));
         course2.add(new Student().new Course(1));
         course3.add(new Student().new Course(2));
-        student[0] = new Student(1, 23, "curry", "male",course1);
-        student[1] = new Student(2, 25, "kobe", "male",course2);
+        student[0] = new Student(1, 23, "curry", "male", course1);
+        student[1] = new Student(2, 25, "kobe", "male", course2);
         student[2] = new Student(3, 21, "taylor", "female", course3);
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.btn_map:
-                testMap();
-                break;
-            case R.id.btn_flatmap:
-                testFlatMap((i++)%3);
-                break;
-            case R.id.btn_flatmap1:
-                testFlatMap(j%3);
-                testFlatMap1(j%3);
-                j++;
-                break;
-            case R.id.btn_filter:
-                int num = (int)(Math.random()*100);
-                Log.d(TAG, "num: " + num);
-                testFilter(num);
-                break;
-        }
-    }
-
-    private void testMap(){
+    private void testMap() {
         Observable.just(new Date()).map(new Func1<Date, String>() {
             @Override
             public String call(Date date) {
@@ -95,12 +71,12 @@ public class RxJavaTransformActivity extends AppCompatActivity implements View.O
             @Override
             public void call(String s) {
                 sb.append(s).append("\n");
-                tv_rx_text.setText(sb.toString());
+                tvRxText.setText(sb.toString());
             }
         });
     }
 
-    private void testFlatMap(int i){
+    private void testFlatMap(int i) {
         Subscriber<Student> subscriber = new Subscriber<Student>() {
             @Override
             public void onCompleted() {
@@ -119,19 +95,19 @@ public class RxJavaTransformActivity extends AppCompatActivity implements View.O
                 String name = student.getName();
                 String sex = student.getSex();
                 sb.append("id: " + id + " age: " + age + " name: " + name + " sex: " + sex + "\n");
-                tv_rx_text.setText(sb.toString());
+                tvRxText.setText(sb.toString());
             }
         };
 
         Observable.just(student[i]).subscribe(subscriber);
     }
 
-    private void testFlatMap1(int i){
+    private void testFlatMap1(int i) {
         Subscriber<Student.Course> subscriber = new Subscriber<Student.Course>() {
             @Override
             public void onCompleted() {
                 sb.append("onCompleted\n");
-                tv_rx_text.setText(sb.toString());
+                tvRxText.setText(sb.toString());
             }
 
             @Override
@@ -142,7 +118,7 @@ public class RxJavaTransformActivity extends AppCompatActivity implements View.O
             @Override
             public void onNext(Student.Course course) {
                 sb.append(course.getCourse() + "\n");
-                tv_rx_text.setText(sb.toString());
+                tvRxText.setText(sb.toString());
             }
         };
 
@@ -154,7 +130,7 @@ public class RxJavaTransformActivity extends AppCompatActivity implements View.O
         }).subscribe(subscriber);
     }
 
-    private void testFilter(int i){
+    private void testFilter(int i) {
         Observable.just(i).filter(new Func1<Integer, Boolean>() {
             @Override
             public Boolean call(Integer integer) {
@@ -165,8 +141,32 @@ public class RxJavaTransformActivity extends AppCompatActivity implements View.O
             @Override
             public void call(Integer integer) {
                 sb.append("Current number: " + integer + "\n");
-                tv_rx_text.setText(sb.toString());
+                tvRxText.setText(sb.toString());
             }
         });
+    }
+
+    @OnClick({R.id.btn_map, R.id.btn_flatmap, R.id.btn_flatmap1, R.id.btn_filter, R.id.btn_take})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_map:
+                testMap();
+                break;
+            case R.id.btn_flatmap:
+                testFlatMap((i++) % 3);
+                break;
+            case R.id.btn_flatmap1:
+                testFlatMap(j % 3);
+                testFlatMap1(j % 3);
+                j++;
+                break;
+            case R.id.btn_filter:
+                int num = (int) (Math.random() * 100);
+                Log.d(TAG, "num: " + num);
+                testFilter(num);
+                break;
+            case R.id.btn_take:
+                break;
+        }
     }
 }
