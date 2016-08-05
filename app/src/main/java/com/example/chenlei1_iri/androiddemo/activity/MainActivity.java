3b8e24e059d27786f145728d4ed03121d42066e2
siguoyi <believemine@163.com>
2016-08-05 15:44:30 +0800

    update MainActivity

--- app/src/main/java/com/example/chenlei1_iri/androiddemo/activity/MainActivity.java
+++ app/src/main/java/com/example/chenlei1_iri/androiddemo/activity/MainActivity.java
@@ -14,6 +14,16 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
+    @BindView(R.id.onTouchDemo)
+    Button onTouchDemo;
+    @BindView(R.id.rxJavaDemo)
+    Button rxJavaDemo;
+    @BindView(R.id.rxImageLoadDemo)
+    Button rxImageLoadDemo;
+    @BindView(R.id.rxTransformDemo)
+    Button rxTransformDemo;
+    @BindView(R.id.btn_butterknife)
+    Button btnButterknife;
     @BindView(R.id.btn_svg)
     Button btnSvg;
     @BindView(R.id.btn_number_picker)
@@ -22,69 +32,39 @@ public class MainActivity extends AppCompatActivity {
     Button btnBinder;
     @BindView(R.id.btn_mvp)
     Button btnMvp;
-
-    private Button btn_onTouch;
-    private Button btn_rxJava;
-    private Button btn_rxImageload;
-    private Button btn_rxJavaTransform;
-    private Button btn_butterknife;
+    @BindView(R.id.btn_ndk)
+    Button btnNdk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
-        btn_onTouch = (Button) findViewById(R.id.onTouchDemo);
-        btn_rxJava = (Button) findViewById(R.id.rxJavaDemo);
-        btn_rxImageload = (Button) findViewById(R.id.rxImageLoadDemo);
-        btn_rxJavaTransform = (Button) findViewById(R.id.rxTransformDemo);
-        btn_butterknife = (Button) findViewById(R.id.btn_butterknife);
-
-        btn_onTouch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent onTouchIntent = new Intent(MainActivity.this, OnTouchActivity.class);
-                startActivity(onTouchIntent);
-            }
-        });
-
-        btn_rxJava.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent rxJavaIntent = new Intent(MainActivity.this, RxJavaActivity.class);
-                startActivity(rxJavaIntent);
-            }
-        });
-
-        btn_rxImageload.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent rxImageLoadIntent = new Intent(MainActivity.this, RxImageLoadActivity.class);
-                startActivity(rxImageLoadIntent);
-            }
-        });
-
-        btn_rxJavaTransform.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent rxJavaTransformIntent = new Intent(MainActivity.this, RxJavaTransformActivity.class);
-                startActivity(rxJavaTransformIntent);
-            }
-        });
-
-        btn_butterknife.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent butterKnifeIntent = new Intent(MainActivity.this, ButterKnifeActivity.class);
-                startActivity(butterKnifeIntent);
-            }
-        });
     }
 
-    @OnClick({R.id.btn_svg, R.id.btn_number_picker, R.id.btn_binder, R.id.btn_mvp})
+    @OnClick({R.id.onTouchDemo, R.id.rxJavaDemo, R.id.rxImageLoadDemo, R.id.rxTransformDemo, R.id.btn_butterknife, R.id.btn_svg, R.id.btn_number_picker, R.id.btn_binder, R.id.btn_mvp, R.id.btn_ndk})
     public void onClick(View view) {
         switch (view.getId()) {
+            case R.id.onTouchDemo:
+                Intent onTouchIntent = new Intent(MainActivity.this, OnTouchActivity.class);
+                startActivity(onTouchIntent);
+                break;
+            case R.id.rxJavaDemo:
+                Intent rxJavaIntent = new Intent(MainActivity.this, RxJavaActivity.class);
+                startActivity(rxJavaIntent);
+                break;
+            case R.id.rxImageLoadDemo:
+                Intent rxImageLoadIntent = new Intent(MainActivity.this, RxImageLoadActivity.class);
+                startActivity(rxImageLoadIntent);
+                break;
+            case R.id.rxTransformDemo:
+                Intent rxJavaTransformIntent = new Intent(MainActivity.this, RxJavaTransformActivity.class);
+                startActivity(rxJavaTransformIntent);
+                break;
+            case R.id.btn_butterknife:
+                Intent butterKnifeIntent = new Intent(MainActivity.this, ButterKnifeActivity.class);
+                startActivity(butterKnifeIntent);
+                break;
             case R.id.btn_svg:
                 Intent svgIntent = new Intent(MainActivity.this, SVGActivity.class);
                 startActivity(svgIntent);
@@ -100,6 +80,10 @@ public class MainActivity extends AppCompatActivity {
             case R.id.btn_mvp:
                 Intent mvpIntent = new Intent(MainActivity.this, MVPActivity.class);
                 startActivity(mvpIntent);
+                break;
+            case R.id.btn_ndk:
+//                Intent ndkIntent = new Intent(MainActivity.this, NDKActivity.class);
+//                startActivity(ndkIntent);
                 break;
         }
     }
