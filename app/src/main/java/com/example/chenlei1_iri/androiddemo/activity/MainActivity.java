d7a965a295b15f378e85187b62138c73984001ea
siguoyi <believemine@163.com>
2016-09-02 17:30:36 +0800

    update toolBar

--- app/src/main/java/com/example/chenlei1_iri/androiddemo/activity/MainActivity.java
+++ app/src/main/java/com/example/chenlei1_iri/androiddemo/activity/MainActivity.java
@@ -1,24 +1,32 @@
 package com.example.chenlei1_iri.androiddemo.activity;
 
-import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ProgressBar;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.example.chenlei1_iri.androiddemo.R;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Toolbar.OnMenuItemClickListener {
     private static final String TAG = MainActivity.class.getSimpleName();
-
     private long firstTime = 0;
 
     @BindView(R.id.onTouchDemo)
@@ -43,16 +51,31 @@ public class MainActivity extends AppCompatActivity {
     Button btnNdk;
     @BindView(R.id.btn_aidl)
     Button btnAidl;
+    @BindView(R.id.btn_date_picker)
+    Button btnDatePicker;
+    @BindView(R.id.pb)
+    ProgressBar pb;
+    private int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.inflateMenu(R.menu.main);
+        toolbar.setOnMenuItemClickListener(this);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 
     @OnClick({R.id.onTouchDemo, R.id.rxJavaDemo, R.id.rxImageLoadDemo, R.id.rxTransformDemo, R.id.btn_butterknife,
-            R.id.btn_svg, R.id.btn_number_picker, R.id.btn_binder, R.id.btn_mvp, R.id.btn_ndk, R.id.btn_aidl})
+            R.id.btn_svg, R.id.btn_number_picker, R.id.btn_binder, R.id.btn_mvp, R.id.btn_ndk, R.id.btn_aidl, R.id.btn_date_picker})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.onTouchDemo:
@@ -99,13 +122,40 @@ public class MainActivity extends AppCompatActivity {
                 Intent aidlIntent = new Intent(MainActivity.this, AIDLActivity.class);
                 startActivity(aidlIntent);
                 break;
+            case R.id.btn_date_picker:
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DATE);
+                int hour = calendar.get(Calendar.HOUR_OF_DAY);
+                int minute = calendar.get(Calendar.MINUTE);
+
+                if (++count % 2 == 1) {
+                    DatePickerDialog datePickerDialog = new DatePickerDialog(this, R.style.MyDialogTheme, new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                            Toast.makeText(MainActivity.this, "The selected date is " + year + "-" + (monthOfYear + 1) + "-" + dayOfMonth, Toast.LENGTH_SHORT).show();
+                        }
+                    }, year, month, day);
+
+                    datePickerDialog.show();
+                } else {
+                    TimePickerDialog timePickerDialog = new TimePickerDialog(this, R.style.MyDialogTheme, new TimePickerDialog.OnTimeSetListener() {
+                        @Override
+                        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                            Toast.makeText(MainActivity.this, "The selected time is " + hourOfDay + ":" + minute, Toast.LENGTH_SHORT).show();
+                        }
+                    }, hour, minute, true);
+                    timePickerDialog.show();
+                    break;
+                }
         }
     }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
 
-        if(keyCode != KeyEvent.KEYCODE_BACK) {
+        if (keyCode != KeyEvent.KEYCODE_BACK) {
             return false;
         }
         long secondTime = System.currentTimeMillis();
@@ -117,5 +167,22 @@ public class MainActivity extends AppCompatActivity {
             System.exit(0);
         }
         return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_favorite:
+                Toast.makeText(this, "Favorite", Toast.LENGTH_SHORT).show();
+                pb.setVisibility(View.VISIBLE);
+                return true;
+
+            case R.id.action_search:
+                Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show();
+                pb.setVisibility(View.GONE);
+                return true;
+        }
+
+        return false;
     }
 }
